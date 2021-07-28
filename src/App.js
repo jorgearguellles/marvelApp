a753be0b17ee35118c8f6aa87f4ec1abbafbe110
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-28 11:49:14 -0500

    add/ .env & useState & useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useState, useEffect } from "react";
 import env from "react-dotenv";
 import { createGlobalStyle } from "styled-components";
 import Home from "./pages/Home/index";
+
+// ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150
 
 
 const Global = createGlobalStyle`
@@ -19,7 +22,21 @@ const Global = createGlobalStyle`
   }
 `;
 
+// const API_KEY = "1a10e24232e5d8541a1fafd20690d17e";
+// const HASH = "68cf490a1bf1fd3b9fa556dd99e6914b";
+
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(
+    () => {
+      fetch(`https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${env.API_KEY}&hash=${env.HASH}`)
+        .then((response) => response.json())
+        .then((data) => setData(data.data.results));
+    },[])
+
+    console.log(data);
+
   return (
     <>
       <Global />
