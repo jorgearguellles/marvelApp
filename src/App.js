9fd3c90f026dfc6254e1033e33a32eca1eae30b5
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-28 19:25:49 -0500

    update/ fecthData.js working and show data in console

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-// import { useState, useEffect } from "react";
-import env from "react-dotenv";
 import Home from "./pages/Home/index";
-import Minicard from "./components/MiniCard";
 import Global from "./globalStyles";
-import FetchData from "./hooks/useGetData";
+import FetchData from "./hooks/fetchData";
 
 function App() {
   const MarvelData = FetchData();
   console.log(MarvelData);
+  if(MarvelData.code === 200){
+    console.log(MarvelData.data.results[0]);
+  }
 
   return (
     <>
       <Global />
       <Home />
-
-      <Minicard />
-      
     </>
   );
 }
