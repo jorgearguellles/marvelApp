662aeabd551cab86cc6cb35c349918dc50ab4205
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-29 16:37:29 -0500

    update/ Home styles fix overflow data show

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,16 +4,19 @@ const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   position: relative;
+  overflow: hidden;
 `;
 
 const WrapperResults = styled.div`
   width: 130rem;
+  height: inherit;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-wrap: wrap;
   margin: 1.5rem auto;
-  overflow: scroll;
+  padding-bottom: 18.3rem;
+  overflow: auto;
 `;
 
 export { Wrapper,WrapperResults};
