34c7115876e51b3562b0e00573af14161244436d
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-07-30 12:51:13 -0500

    add/ NotFound Page done

---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,8 @@
+
+function NotFound(){
+  return(
+      <h1>Oops ... your hero is not here</h1>
+  )
+};
+
+export default NotFound;
--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,18 @@
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter,Switch ,Route} from "react-router-dom";
+import Global from "../globalStyles";
 import Home from "../pages/Home/index";
 import Login from "../pages/Login"
-import Global from "../globalStyles";
+import NotFound from "../pages/NotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Global />
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/login" component={Login}/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/login" component={Login}/>
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
