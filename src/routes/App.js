fc5d8ee64944083c85ae5a5374939f2d97112de6
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-08-03 17:51:45 -0500

    add/ Hero route

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,9 +1,10 @@
 import {BrowserRouter,Switch ,Route} from "react-router-dom";
 import Global from "../globalStyles";
 import Home from "../pages/Home/index";
-import Login from "../pages/LogIn"
+import LogIn from "../pages/LogIn"
 import NotFound from "../pages/NotFound";
 import SignUp from "../pages/SignUp";
+import HeroDetails from "../pages/HeroDetails";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
       <Global />
       <Switch>
         <Route exact path="/" component={Home}/>
-        <Route exact path="/login" component={Login}/>
+        <Route exact path="/login" component={LogIn}/>
         <Route exact path="/signup" component={SignUp}/>
+        <Route exact path="/hero" component={HeroDetails}/>
         <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
