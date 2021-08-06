717bb83a569249485281a8ed38249eedbaecb39e
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-08-05 22:59:04 -0500

    update/ firebase config

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -2,9 +2,7 @@ import env from "react-dotenv";
 import firebase from "firebase/app";
 import "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+const app = firebase.initializeApp({
   apiKey: `${env.apiKey}`,
   authDomain: `${env.authDomain}`,
   projectId: `${env.projectId}`,
@@ -12,9 +10,7 @@ const firebaseConfig = {
   messagingSenderId: `${env.messagingSenderId}`,
   appId: `${env.appId}`,
   measurementId: `${env.measurementId}`
-};
-// Initialize Firebase
-const app = firebase.initializeApp(firebaseConfig);
+});
 
 export const auth = app.auth();
 export default app;
