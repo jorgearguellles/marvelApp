6e3acf02f9d7f93d57a65501c08f15390bdeea5d
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2021-08-06 19:25:41 -0500

    update/ firebase conecction rady to get moon

---
+++ public/env.js
@@ -0,0 +1,11 @@
+window.env = {
+  "API_KEY": "1a10e24232e5d8541a1fafd20690d17e",
+  "HASH": "68cf490a1bf1fd3b9fa556dd99e6914b",
+  "apiKey": "AIzaSyBrovUb-nVpAhBhJhiCT8cYsT_vidBjZks",
+  "authDomain": "marvelapp-44961.firebaseapp.com",
+  "projectId": "marvelapp-44961",
+  "storageBucket": "marvelapp-44961.appspot.com",
+  "messagingSenderId": "391817787314",
+  "appId": "1:391817787314:web:ac65cd9527fdd6ae8296ab",
+  "measurementId": "G-N2S6EGZCJ8"
+};
--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -18,3 +18,6 @@ const useGetData = () => {
 };
 
 export default useGetData;
+
+
+
